a9485bebce862d11e20c3feed977fe882be4803c
GeorgeGhrayib <georgeghrayib2008@gmail.com>
2024-07-29 11:42:30 +0300

    email sender with data base

--- app.py
+++ app.py
@@ -62,42 +62,51 @@ def index():
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     if request.method == 'GET':
-        return render_template("dashboard.html") 
+        return render_template("dashboard.html",emails = db.child("emails").get().val()) 
     elif request.form['action'] == "mailsender":
         subject = request.form['subject']
         body = request.form['message']
         file = request.form['file']
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = "makeoveryourleftovers@gmail.com"
-        msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'plain'))
-        attachment = MIMEBase('application', 'octet-stream')
-        try:
-            with open(file, 'rb') as file:
-                attachment.set_payload(file.read())
-                encoders.encode_base64(attachment)
-                attachment.add_header('Content-Disposition', f'attachment; filename={file}')
-                msg.attach(attachment)
-        except Exception as e:
-            print(f"Failed to attach file {file}. Error: {str(e)}")
-
-        # Send the email
-        try:
-            with smtplib.SMTP(smtp_server, smtp_port) as server:
-                server.starttls()  # Secure the connection
-                server.login(sender_email, password)
-                server.sendmail(sender_email, msg['To'], msg.as_string())
-                print(f"Email sent to {msg['To']}")
-                return redirect(url_for('dashboard'))
-        except Exception as e:
-            print(f"Failed to send email to {msg['To']}. Error: {str(e)}")
-            return redirect(url_for('dashboard'))
+        for email in db.child("emails").get().val():
+            send_email(email,subject,body,file)
+        return redirect(url_for('dashboard'))
+    elif request.form['action'] == 'signout':
+        auth.current_user = None
+        return redirect(url_for('index'))
     else:
         emails = db.child("emails").get().val()
         emails.append(request.form['email'])
         db.child("emails").set(emails)
         return redirect(url_for('dashboard'))
+    
+def send_email(recipient_email,subject,body,file):
+    # Create the email message
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    # Attach the file
+    attachment = MIMEBase('application', 'octet-stream')
+    try:
+        with open(file, 'rb') as file:
+            attachment.set_payload(file.read())
+            encoders.encode_base64(attachment)
+            attachment.add_header('Content-Disposition', f'attachment; filename={file}')
+            msg.attach(attachment)
+    except Exception as e:
+        print(f"Failed to attach file {file}. Error: {str(e)}")
+
+    # Send the email
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()  # Secure the connection
+            server.login(sender_email, password)
+            server.sendmail(sender_email, recipient_email, msg.as_string())
+            print(f"Email sent to {recipient_email}")
+    except Exception as e:
+        print(f"Failed to send email to {recipient_email}. Error: {str(e)}")
             
 @app.route('/calender', methods=['GET', 'POST'])
 def calender():
